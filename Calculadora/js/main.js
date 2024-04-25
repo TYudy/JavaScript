fa8904b711c6026282bfb33c1b270ca816f2487f
l <cn.t0604@gmail.com>
2024-04-25 09:28:34 -0500

    Operaciones

--- Calculadora/js/main.js
+++ Calculadora/js/main.js
@@ -1,6 +1,16 @@
 var po={
     teclas: document.querySelectorAll("#container ul li"),
-    action: null
+    action: null,
+    digito: null,
+    operaciones: document.querySelector("#operaciones"),
+    cantising: 0,
+    cantidecimal: false,
+    resultado:false
+
+
+
+
+
 }
 
 var fun = {
@@ -16,42 +26,89 @@ var fun = {
         fun.calculadora(po.action,po.digito);
 
     },
-    calculadora:function(action){
+    calculadora:function(action,digito){
 
         switch(action){
             case "numero":
                 po.cantising = 0;
                 if(po.operaciones.innerHTML == 0){
-                    po.operaciones.innerHTML = 0;
+                    po.operaciones.innerHTML = digito;
+                    
+                }
+                else{
+                    if(po.resultado){
+                        po.resultado = false;
+                        po.operaciones.innerHTML = digito;
+                    }
+                    else{
+                        po.operaciones.innerHTML += digito;
+                        po.cantidecimal = false;
+                        po.resultado = false;
+                    }
                 }
                 console.log("numero");
             break;
             case "simbolo":
+                po.cantising ++;
+                if(po.cantising == 1){
+                    if(po.operaciones.innerHTML == 0){
+                        po.operaciones.innerHTML = 0;
+                        
+                
+                    }
+                    else{
+                        po.operaciones.innerHTML += digito;
+                    }
+                }
                 console.log("simbolo");
             break;
             case "raiz":
+                po.operaciones.innerHTML = Math.sqrt(po.operaciones.innerHTML);
+                po.resultado = true;
                 console.log("raiz");
             break;
             case "cubo":
+                po.operaciones.innerHTML = Math.pow(po.operaciones.innerHTML,3);
+                po.resultado = true;
                 console.log("cubo");
             break;
-            case "igual":
-                console.log("igual");
-            break;
+            
             case "decimal":
+                if(!po.cantising && po.cantising!=1){
+                    po.operaciones.innerHTML += digito;
+                    po.cantidecimal = true;
+                    po.resultado = false;
+                }
                 console.log("decimal");
             break;
             case "pi":
+                po.operaciones.innerHTML = Math.PI*(po.operaciones.innerHTML);
+                po.resultado = true;
                 console.log("pi");
             break;
-            case "decimal":
-                console.log("decimal");
+            
+            case "igual":
+                po.operaciones.innerHTML = eval(po.operaciones.innerHTML);
+                po.resultado = true;
+                console.log("igual");
             break;
-            case "decimal":
-                console.log("decimal");
+            case "eliminar":
+                
+                if(po.operaciones.innerHTML.length <= 1){
+                    po.operaciones.innerHTML = 0;
+                }else{
+                    po.operaciones.innerHTML = po.operaciones.innerHTML.slice(0, -1) ;
+                }
+               
+                console.log("eliminar");
             break;
+            
         }
 
+    },
+    borrarCalculadora:function(){
+        po.resultado = false;
+        po.operaciones.innerHTML = 0;
     }
 }
 fun.inicio();
--- NumM.js
+++
@@ -1,8 +0,0 @@
-let Numeros =  [10,5,8,20,3,15];
-let NumM = Math.max(...Numeros)
-document.write(`El array es: ${Numeros} <br> El numero mayor es ${NumM} <br>`);
-
-
-const Numerosp =  [10,5,8,20,3,15];
-const NumMp = Math.max(Numerosp)
-document.write(`El array es: ${Numerosp} <br> El numero mayor es ${NumMp}`);
